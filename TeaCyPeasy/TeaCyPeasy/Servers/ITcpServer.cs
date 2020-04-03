71917cfde2307c0d6d12fc307a940a29bbc6b68c
BanallyMe <admin@eggrade.de>
2020-04-03 15:12:25 +0200

    Added first interface for a TCP server.

---
+++ TeaCyPeasy/TeaCyPeasy/Servers/ITcpServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BanallyMe.TeaCyPeasy.Servers
+{
+    /// <summary>
+    /// API for a TeaCyPeasy server.
+    /// </summary>
+    public interface ITcpServer : IDisposable
+    {
+        /// <summary>
+        /// Tells the server to start listening for incoming connections.
+        /// </summary>
+        Task StartServer();
+
+        /// <summary>
+        /// Tells the server to stop listening for incoming connections.
+        /// </summary>
+        void StopServer();
+    }
+}
