14267dfc33577eea7326f503a467f8472a4b8ccd
BanallyMe <admin@eggrade.de>
2020-04-03 17:43:03 +0200

    Added basic routines to check if server has been started or stopped.

---
+++ TeaCyPeasy/TeaCyPeasy.UnitTests/Servers/NetTcpListenerBasedTcpServerTests.cs
@@ -0,0 +1,53 @@
+﻿using BanallyMe.TeaCyPeasy.Exceptions;
+using BanallyMe.TeaCyPeasy.Servers;
+using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BanallyMe.TeaCyPeasy.UnitTests.Servers
+{
+    public class NetTcpListenerBasedTcpServerTests
+    {
+        [Fact]
+        public async Task StartServer_ThrowsExceptionIfServerIsAlreadyStarted()
+        {
+            using var testedServer = new NetTcpListenerBasedTcpServer();
+            Func<Task> starting = async () => await testedServer.StartServer();
+            await starting();
+
+            await starting.Should().ThrowExactlyAsync<TcpServerStartException>().WithMessage("This TCP server has already been started.");
+        }
+
+        [Fact]
+        public async Task StartServer_CanBeStartedAgainAfterHasBeenStopped()
+        {
+            using var testedServer = new NetTcpListenerBasedTcpServer();
+            Func<Task> starting = async () => await testedServer.StartServer();
+            await starting();
+            testedServer.StopServer();
+
+            await starting.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public void StopServer_ThrowsExceptionIfServerWasNotStarted()
+        {
+            using var testedServer = new NetTcpListenerBasedTcpServer();
+            Action stopping = () => testedServer.StopServer();
+
+            stopping.Should().ThrowExactly<TcpServerStopException>().WithMessage("This TCP server has not been started yet.");
+        }
+
+        [Fact]
+        public async Task StopServer_ThrowsExceptionIfServerIsStoppedTwice()
+        {
+            using var testedServer = new NetTcpListenerBasedTcpServer();
+            Action stopping = () => testedServer.StopServer();
+            await testedServer.StartServer();
+            stopping();
+
+            stopping.Should().ThrowExactly<TcpServerStopException>().WithMessage("This TCP server has not been started yet.");
+        }
+    }
+}
---
+++ TeaCyPeasy/TeaCyPeasy/Servers/NetTcpListenerBasedTcpServer.cs
@@ -0,0 +1,49 @@
+﻿using BanallyMe.TeaCyPeasy.Exceptions;
+using System;
+using System.Threading.Tasks;
+
+namespace BanallyMe.TeaCyPeasy.Servers
+{
+    /// <summary>
+    /// Implementation for a TeaCyPeasy TCP server which is using the .NET
+    /// TcpListener functions for communicating to clients.
+    /// </summary>
+    public class NetTcpListenerBasedTcpServer : ITcpServer
+    {
+        private bool serverIsRunning;
+
+        public async Task StartServer()
+        {
+            if(serverIsRunning)
+            {
+                throw new TcpServerStartException(this, "This TCP server has already been started.");
+            }
+            serverIsRunning = true;
+        }
+
+        public void StopServer()
+        {
+            if(!serverIsRunning)
+            {
+                throw new TcpServerStopException(this, "This TCP server has not been started yet.");
+            }
+            serverIsRunning = false;
+        }
+
+
+        private bool isAlreadyDisposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool calledFromDispose)
+        {
+            if (isAlreadyDisposed) return;
+
+            isAlreadyDisposed = true;
+        }
+    }
+}
