c17a29b89daa7b6775270c7ee500dd4109bc39df
BanallyMe <admin@eggrade.de>
2020-04-03 16:39:48 +0200

    Added exception for errors in stopping a TCP server

---
+++ TeaCyPeasy/TeaCyPeasy/Exceptions/TcpServerStopException.cs
@@ -0,0 +1,22 @@
+﻿using BanallyMe.TeaCyPeasy.Servers;
+using System;
+
+namespace BanallyMe.TeaCyPeasy.Exceptions
+{
+    /// <summary>
+    /// Exception which is related to starting up a TCP server.
+    /// </summary>
+#pragma warning disable CA1032 // No standard constructors, as a related TCP server always has to be passed.
+    public class TcpServerStopException : TcpServerException
+#pragma warning restore CA1032
+    {
+        public TcpServerStopException(ITcpServer relatedTcpServer)
+            : base(relatedTcpServer) { }
+
+        public TcpServerStopException(ITcpServer relatedTcpServer, string message)
+            : base(relatedTcpServer, message) { }
+
+        public TcpServerStopException(ITcpServer relatedTcpServer, string message, Exception innerException)
+            : base(relatedTcpServer, message, innerException) { }
+    }
+}
