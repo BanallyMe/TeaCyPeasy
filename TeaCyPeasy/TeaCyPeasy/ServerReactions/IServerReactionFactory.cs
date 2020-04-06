cc726be9e0111d0498dbf7e6353eb2e9a55547b9
BanallyMe <admin@eggrade.de>
2020-04-06 09:02:33 +0200

    Interface for creating a server reaction

---
+++ TeaCyPeasy/TeaCyPeasy/ServerReactions/IServerReactionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace BanallyMe.TeaCyPeasy.ServerReactions
+{
+    /// <summary>
+    /// Factory for creating a TCP servers' reaction to a received message.
+    /// </summary>
+    public interface IServerReactionFactory
+    {
+        /// <summary>
+        /// Creates a stream, that can be sent to a client as an answer to a received message from a client.
+        /// </summary>
+        /// <param name="receivedData">Data that has been received from the client.</param>
+        /// <returns>The stream that can be sent to the client. Null if no stream could be created.</returns>
+        Stream? CreateServerReaction(Stream receivedData);
+    }
+}
