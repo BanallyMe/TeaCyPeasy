7b6190635b76606b59880264e0a6c0539f25c2c1
BanallyMe <admin@eggrade.de>
2020-04-06 10:31:44 +0200

    Deleted reaction factory in favour of a provider which can also determine which factory should be used.

---
+++ TeaCyPeasy/TeaCyPeasy/ServerReactions/IServerReactionProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace BanallyMe.TeaCyPeasy.ServerReactions
+{
+    /// <summary>
+    /// Factory for creating a TCP servers' reaction to a received message.
+    /// </summary>
+    public interface IServerReactionProvider
+    {
+        /// <summary>
+        /// Creates a stream, that can be sent to a client as an answer to a received message from a client.
+        /// </summary>
+        /// <param name="receivedData">Data that has been received from the client.</param>
+        /// <returns>The stream that can be sent to the client. Null if no stream could be created.</returns>
+        Stream? CreateServerReaction(Stream receivedData);
+
+        /// <summary>
+        /// Registers a factory method for creating a server reaction. Uses a conditional delegate that can determine the cases
+        /// when this factory method should be used.
+        /// </summary>
+        /// <param name="reactionFactory">The factory method for creating the servers reaction to a received message.</param>
+        /// <param name="conditionDelegate">
+        ///     A delegate to determine if the factory method should be used. Factory method is only used if this delegate returns true.
+        /// </param>
+        void RegisterServerReactionFactoryForCondition(Func<Stream> reactionFactory, Func<Stream, bool> conditionDelegate);
+    }
+}
