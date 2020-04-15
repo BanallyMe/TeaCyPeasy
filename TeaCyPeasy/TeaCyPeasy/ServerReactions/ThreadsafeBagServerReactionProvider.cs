e2289b2af2c6f214bee9b1b2d5f038182e708152
BanallyMe <admin@eggrade.de>
2020-04-15 10:57:51 +0200

    Create a threadsafe implementation of an IServerReactionProvider

---
+++ TeaCyPeasy/TeaCyPeasy/ServerReactions/ThreadsafeBagServerReactionProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace BanallyMe.TeaCyPeasy.ServerReactions
+{
+    /// <summary>
+    /// An implementation for an IServerReactionProvider which is using a threadsafe bag for storing the factory methods
+    /// that create reactions sent from the server.
+    /// </summary>
+    internal class ThreadsafeBagServerReactionProvider : IServerReactionProvider
+    {
+        /// <inheritdoc />
+        public Stream? CreateServerReaction(Stream receivedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void RegisterServerReactionFactoryForCondition(Func<Stream> reactionFactory, Func<Stream, bool> conditionDelegate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
